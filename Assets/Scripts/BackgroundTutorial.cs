d752eefaf0117165166b5c0ef858373e304e4b63
XINWUYA <xinwuya_scu@163.com>
2019-11-12 22:03:54 +0800

    完成ray tracing第二部分的功能

---
+++ Assets/Scripts/BackgroundTutorial.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BackgroundTutorial : RayTracingTutorial
+{
+    public BackgroundTutorial(RayTracingTutorialAsset vAsset) : base(vAsset)
+    { }
+
+    public override void Render(ScriptableRenderContext vContext, Camera vCamera)
+    {
+        base.Render(vContext, vCamera);
+
+        var RenderTarget = RequireRenderTarget(vCamera);
+
+        var Command = CommandBufferPool.Get(typeof(BackgroundTutorial).Name);
+        try
+        {
+            using (new ProfilingSample(Command, "RayTracing"))
+            {
+                Command.SetRayTracingTextureParam(_rayTracingShader, _renderTargetShaderId, RenderTarget);
+                Command.DispatchRays(_rayTracingShader, "BackgroundRaygenShader", (uint)RenderTarget.rt.width, (uint)RenderTarget.rt.height, 1, vCamera);
+            }
+            vContext.ExecuteCommandBuffer(Command);
+
+            using (new ProfilingSample(Command, "FinalBlit"))
+            {
+                Command.Blit(RenderTarget, BuiltinRenderTextureType.CameraTarget, Vector2.one, Vector2.zero);
+            }
+            vContext.ExecuteCommandBuffer(Command);
+        }
+        finally
+        {
+            CommandBufferPool.Release(Command);
+        }
+    }
+}
---
+++ Assets/Scripts/BackgroundTutorialAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BackgroundTutorialAsset", menuName = "RayTracingRendering/BackgroundTutorialAsset")]
+public class BackgroundTutorialAsset : RayTracingTutorialAsset
+{
+    public override RayTracingTutorial CreateTutorial()
+    {
+        return new BackgroundTutorial(this);
+    }
+}
